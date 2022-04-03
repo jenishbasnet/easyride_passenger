68b9ae9645332aebb4fa9bbc21af0329fa3dbf8b
jenishbasnet <jenissh.basnet@gmail.com>
2022-04-03 19:08:00 +0545

    Jenish Static

--- lib/Screens/profile.dart
+++ lib/Screens/profile.dart
@@ -14,6 +14,7 @@ import '../requests/baseurl.dart';
 class Profile extends StatelessWidget {
   static String? emailHolder;
   static String? passwordHolder;
+  static String? jenish;
 
   @override
   Widget build(BuildContext context) {
